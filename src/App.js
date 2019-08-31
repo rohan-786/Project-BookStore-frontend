398dbb2f0561133138253e60e5a29843f885cc34
Rohan.choudhary <rohan.choudhary@99acres.com>
2019-09-01 01:18:57 +0530

    MERGE : .babelrc file added in project 1. Boilder plate working successfully. 2. Hot reloding working. 3. Dummy commponent added and tested.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 class App extends React.Component {
     render() { 
-        return (<div>Hello world!
-            
+        return (<div>Hello world! I am Rohan
+
         </div>  );
     }
 }
