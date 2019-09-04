164ee8248de104c377185d59f971170e2ff9db5a
rohan <rohan.choudhary>
2019-09-04 09:56:15 +0530

    MERGE : basic setup done

--- src/client.js
+++ src/client.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Provider} from 'react-redux';
 
 
-ReactDOM.render(<App/> , document.getElementById('app'));
+
+
+ReactDOM.render(
+<App/> , document.getElementById('app'));
